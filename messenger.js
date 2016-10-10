8eafa7644dff0ee43bce612c62c2e7c230dec7b6
sriharan40 <sriharan40@gmail.com>
2016-10-11 01:36:13 +0530

    Update messenger.js

--- messenger.js
+++ messenger.js
@@ -1,7 +1,7 @@
 var FBMessenger = require('fb-messenger')
-var messenger = new FBMessenger("EAAEcEkKVmnIBAORKWxp0nQh9ZACPidqF80f4gPnNfHT4CpY5plHnnMCHhll29szbtQynK2wyIND3P2MiPfDX5A267cMD9aWc0mih7fFneknBqvpOUOvjZCgFBsoUrorCgH6ZBxI4eRD6Q1lwSAQD20AMwabphBqMWzATmxHDAZDZD")
+var messenger = new FBMessenger('EAAEcEkKVmnIBAORKWxp0nQh9ZACPidqF80f4gPnNfHT4CpY5plHnnMCHhll29szbtQynK2wyIND3P2MiPfDX5A267cMD9aWc0mih7fFneknBqvpOUOvjZCgFBsoUrorCgH6ZBxI4eRD6Q1lwSAQD20AMwabphBqMWzATmxHDAZDZD')
 
-messenger.sendTextMessage("8050582590", 'Hello', function (err, body) {
+messenger.sendTextMessage('sri.haran.14', 'Hello', function (err, body) {
   if(err) return console.error(err)
   console.log(body)
 })
