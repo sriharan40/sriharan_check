2ad43636b5348af311cc4b343d94b7771dd72c51
sriharan40 <sriharan40@gmail.com>
2016-10-12 09:19:33 +0530

    Update message.js

--- message.js
+++ message.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var FBMessenger = require('fb-messenger');
-var request = require("request");
-
 var app = express();
+var request = require("request");
+var FBMessenger = require('fb-messenger');
+
 
 var token = "EAACXZB4I0ilMBALixoOmi8ZBvJOFedZCREJcZAavo5q91KBwsc19ngPftSdTiTRcZA8R9eGCgQNFx1Xz0AiTF2fUGpAvZA9M0tlzDlQW3KpXoD1Jy5Q0OaI3raYkGktd2mVP7a5wVZBKKb91BevdPc3Qr4GJb5npQbm8Rp1kerecgZDZD";
 
