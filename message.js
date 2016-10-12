acaf932c15925786943df128b991fc6fb8d70702
sriharan40 <sriharan40@gmail.com>
2016-10-12 23:09:37 +0530

    Add files via upload

--- message.js
+++ message.js
@@ -61,6 +61,6 @@ function sendTextMessage(sender, text) {
     res.write(JSON.stringify(responseBody));
     res.end();
 
-)};
+});
 
 }).listen((process.env.PORT), () => console.log("Server listening"));
