63a3bc9ae1d98c10e03c264c0ae3f51faf2adcd9
sriharan40 <sriharan40@gmail.com>
2016-10-13 08:42:25 +0530

    Add files via upload

--- message.js
+++ message.js
@@ -32,30 +32,17 @@ req.params=params(req);
 
 var token = "EAAEcEkKVmnIBAChlOhWc1tHveQIHOuutAOQQGAQqL7QbwPXBO5zC0pOG39JmHsOl81UZA6W3C4wZAZBf9z4l88RKEacF7zg65NWyGoBr4b6vmLoTLQuUXlBSI21IohuSU4G0AyJ12F5037LBNndmXotz9xZAq2p3GVZBcNmyIcgZDZD";
 // +639178313417  639178483863
-var sender = "+918050582590";
+//var sender = "+918050582590";
 //var sender = "himant.gupta";
-var text = "Auto message on your number +91 8050582590";
+//var text = "Auto message on your number +91 8050582590";
 
 //app.get('/', function(req, res){
 
 //var query = url_query.query;
 
 var sender = req.params.mobile;
-//var sender = "himant.gupta";
+
 var text = req.params.message;
-
-var speech = 'Message sent successfully to '+sender;	
-
-res.statusCode = 200;
-	
-res.setHeader('Content-Type', 'application/json');	
-
-// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
-     var responseBody = {
-        "speech": speech,
-        "displayText": speech,	     
-        "source": "apiai-Himant-message sample"
-    };
 	
 sendTextMessage(sender, text);
 
@@ -74,11 +61,27 @@ function sendTextMessage(sender, text) {
   }, function(error, res, body) {
     if (error) {
       console.log('Error sending message: ', error);
+	  var speech = error;
     } else if (res.body.error) {
       console.log('Error: ', res.body.error);
+	  var speech = res.body.error;
     }
+	else{
+      var speech = 'Message sent successfully to '+sender;			
+	}
   });
 }
+
+res.statusCode = 200;
+	
+res.setHeader('Content-Type', 'application/json');	
+
+// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
+     var responseBody = {
+        "speech": speech,
+        "displayText": speech,	     
+        "source": "apiai-Himant-message sample"
+    };
 
     res.write(JSON.stringify(responseBody));
     res.end();
