fc6535c7216d767417fd6eadd8e2f950f4526d61
sriharan40 <sriharan40@gmail.com>
2016-10-20 09:56:06 +0530

    Update message.js

--- message.js
+++ message.js
@@ -30,7 +30,7 @@ req.params=params(req);
 
 //var url_query = require('url').parse(request.url).pathname;
 
-var token = "EAAEcEkKVmnIBAChlOhWc1tHveQIHOuutAOQQGAQqL7QbwPXBO5zC0pOG39JmHsOl81UZA6W3C4wZAZBf9z4l88RKEacF7zg65NWyGoBr4b6vmLoTLQuUXlBSI21IohuSU4G0AyJ12F5037LBNndmXotz9xZAq2p3GVZBcNmyIcgZDZD";
+var token = process.env.FB_PAGE_TOKEN;
 // +639178313417  639178483863
 //var sender = "+918050582590";
 //var sender = "himant.gupta";
@@ -40,7 +40,7 @@ var token = "EAAEcEkKVmnIBAChlOhWc1tHveQIHOuutAOQQGAQqL7QbwPXBO5zC0pOG39JmHsOl81
 
 //var query = url_query.query;
 
-var sender = req.params.mobile;
+var sender = req.params.sender;
 
 var text = req.params.message;
 
@@ -53,15 +53,27 @@ sendTextMessage(sender, text, res);
 
 function sendTextMessage(sender, text, res) {
 
-  messageData = {
-    text:text
-  }
-  request({
+  messageData = 
+    "text":"Select an option:",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"My Outstanding Balance",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        "content_type":"text",
+        "title":"Bills",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
+   
+   request({
       url: 'https://graph.facebook.com/v2.6/me/messages',
       qs: {access_token:token},
       method: 'POST',
       json: {
-        recipient: {phone_number:sender},
+        recipient: {id:sender},
         message: messageData,
       }
   }, function(error, response, body) {
@@ -99,23 +111,7 @@ function sendTextMessage(sender, text, res) {
     res.write(JSON.stringify(responseBody));
     res.end();
 	  }
-	else{
-	  var speech = 'Message sent successfully to '+sender;				
-		
-	  res.statusCode = 200;
-			
-      res.setHeader('Content-Type', 'application/json');	
-
-// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
-     var responseBody = {
-        "speech": speech,
-        "displayText": speech,	     
-        "source": "apiai-Himant-message sample"
-    };
-
-    res.write(JSON.stringify(responseBody));
-    res.end();	
-	}  
+  
 	  });
 	  }
 
