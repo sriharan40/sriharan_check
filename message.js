609f59727e9cd7669b4e21d7106a2ad448039a76
sriharan40 <sriharan40@gmail.com>
2016-10-22 02:19:59 +0530

    Update message.js

--- message.js
+++ message.js
@@ -44,19 +44,127 @@ var sender = req.params.sender;
 
 var mobile = req.params.mobile;
 
+var text = req.params.message;
+
+var payment = req.params.payment;
+
 //var speech = 'Message sent successfully to '+sender;			
 
-if(sender)
-{	
-sendTextMessage(sender, res);
+if(payment)
+{
+var text = "Congratulations your payment done successfully.";
+	
+sendNotification(payment,text,res);	
 }
 
-if(mobile)
+if(sender && text)
 {	
-sendMessage(mobile, res);
+sendTextMessage(sender, text, res);
 }
 
-function sendMessage(mobile, res) {
+if(mobile && text)
+{	
+sendMessage(mobile, text, res);
+}
+
+function sendNotification(receiver, text, res) {
+
+console.log('Receiver: '+receiver);
+
+messageData = {
+"attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"receipt",
+        "recipient_name":"Sriharan",		
+        "order_number":"12345678902",
+        "currency":"USD",
+        "payment_method":"PayPal",        
+        "timestamp":"1428444852", 
+        "elements":[
+          {
+            "title":"Congratulations for the Payment",
+            "subtitle":"Payment Success",
+            "quantity":1,
+            "price":1,
+            "currency":"USD",
+          }
+		 ],
+        "summary":{
+          "subtotal":1.00,
+          "total_cost":1.00
+        }
+      }
+    }
+  }
+   
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+	    recipient: {id:receiver},
+        message: messageData,
+      }
+  }, function(error, response, body) {
+  console.log('Result: '+receiver);
+  if (error) {
+      console.log('Error sending message: ', error);
+
+	  res.statusCode = 200;
+			
+      res.setHeader('Content-Type', 'application/json');	
+
+// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
+     var responseBody = {
+        "speech": error,
+        "displayText": error,	     
+        "source": "apiai-Himant-message sample"
+    };
+
+    res.write(JSON.stringify(responseBody));
+    res.end();
+
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+
+	  res.statusCode = 200;
+			
+      res.setHeader('Content-Type', 'application/json');	
+
+// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
+     var responseBody = {
+        "speech": response.body.error,
+        "displayText": response.body.error,	     
+        "source": "apiai-Himant-message sample"
+    };
+
+    res.write(JSON.stringify(responseBody));
+    res.end();
+	  }
+	 else{
+		console.log('Result: '+receiver);
+
+   	      res.statusCode = 200;
+	
+		  res.setHeader('Content-Type', 'application/json');
+	  
+	var responseBody = {
+        "speech": text,
+        "displayText": text,	 
+        "source": "apiai-Himant-OTP sample"
+    };
+	
+    res.write(JSON.stringify(responseBody));
+    res.end();
+	
+		 }	 
+  });	  
+	  	  
+}
+
+
+function sendMessage(mobile, text, res) {
 	
 	var accountSid = process.env.accountSid;
 	var authToken = process.env.authToken;
@@ -87,7 +195,7 @@ function sendMessage(mobile, res) {
 	});	
 }
 
-function sendTextMessage(sender, res) {
+function sendTextMessage(sender, text, res) {
 
   messageData = {
     "text":"Select an option:",
