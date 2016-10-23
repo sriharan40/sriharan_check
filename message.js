61dafbcc2d40fee1783e6f719809281b5156283b
sriharan40 <sriharan40@gmail.com>
2016-10-23 15:49:31 +0530

    Update message.js

--- message.js
+++ message.js
@@ -3,9 +3,7 @@ var app = express();
 var mysql = require('mysql');
 var request = require("request");
 var util = require("util");
-//var url  = require('url');
 var http = require('http');
-//var qs = require('querystring');
 
 var db_config = {
     host: 'us-cdbr-iron-east-04.cleardb.net',
@@ -24,14 +22,12 @@ var connection;
     connection.connect(function(err) {              	// The server is either down
         if (err) {                                     // or restarting (takes a while sometimes).
             console.log('2. error when connecting to db:', err);
-            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
         }                                     	// to avoid a hot loop, and to allow our node script to
     });                                     	// process asynchronous requests in the meantime.
     											// If you're also serving http, display a 503 error.
     connection.on('error', function(err) {
         console.log('3. db error', err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
-            handleDisconnect();                      	// lost due to either server restart, or a
         } else {                                      	// connnection idle timeout (the wait_timeout
             throw err;                                  // server variable configures this)
         }
