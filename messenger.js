7e744f6975eec04f308f14fd4a050f0a17093f05
sriharan40 <sriharan40@gmail.com>
2016-10-24 22:45:46 +0530

    Delete messenger.js

--- messenger.js
+++
@@ -1,7 +0,0 @@
-var FBMessenger = require('fb-messenger');
-var messenger = new FBMessenger('EAACXZB4I0ilMBALixoOmi8ZBvJOFedZCREJcZAavo5q91KBwsc19ngPftSdTiTRcZA8R9eGCgQNFx1Xz0AiTF2fUGpAvZA9M0tlzDlQW3KpXoD1Jy5Q0OaI3raYkGktd2mVP7a5wVZBKKb91BevdPc3Qr4GJb5npQbm8Rp1kerecgZDZD');
-
-messenger.sendTextMessage('8050582590', 'Hello', function (err, body) {
-  if(err) return console.error(err);
-  console.log(body);
-})
