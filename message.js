893bbecfedb8a735bb79302dd1f3bce1bdf07998
sriharan40 <sriharan40@gmail.com>
2016-10-25 00:53:30 +0530

    Update message.js

--- message.js
+++ message.js
@@ -110,6 +110,82 @@ function sendNotification(receiver, text, res) {
 
 console.log('Receiver: '+receiver);
 
+messageData1 = {
+    "text":"Will you get in touch with us for future payments also ?",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Yes",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        "content_type":"text",
+        "title":"No",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
+   }
+   
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+	    recipient: {id:receiver},
+        message: messageData1,
+      }
+  }, function(error, response, body) {
+  console.log('Result: '+receiver);
+  if (error) {
+      console.log('Error sending message: ', error);
+
+	  res.statusCode = 200;
+			
+      res.setHeader('Content-Type', 'application/json');	
+
+// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
+     var responseBody = {
+        "speech": error,
+        "displayText": error,	     
+        "source": "apiai-Himant-message sample"
+    };
+
+    res.write(JSON.stringify(responseBody));
+    res.end();
+
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+
+	  res.statusCode = 200;
+			
+      res.setHeader('Content-Type', 'application/json');	
+
+// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
+     var responseBody = {
+        "speech": response.body.error,
+        "displayText": response.body.error,	     
+        "source": "apiai-Himant-message sample"
+    };
+
+    res.write(JSON.stringify(responseBody));
+    res.end();
+	  }
+	 else{
+		console.log('Result: '+receiver);
+
+   	      res.statusCode = 200;
+	
+		  res.setHeader('Content-Type', 'application/json');
+	  
+	var responseBody = {
+        "speech": text,
+        "displayText": text,	 
+        "source": "apiai-Himant-OTP sample"
+    };	
+		 }	 
+  });
+  
+
 messageData = {
 "attachment":{
       "type":"template",
@@ -194,88 +270,11 @@ messageData = {
         "source": "apiai-Himant-OTP sample"
     };
 }
-	 });
-  	 
-messageData1 = {
-    "text":"Will you get in touch with us for future payments also ?",
-    "quick_replies":[
-      {
-        "content_type":"text",
-        "title":"Yes",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-      },
-      {
-        "content_type":"text",
-        "title":"No",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-      }
-    ]
-   }
-   
-  request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token:token},
-      method: 'POST',
-      json: {
-	    recipient: {id:receiver},
-        message: messageData1,
-      }
-  }, function(error, response, body) {
-  console.log('Result: '+receiver);
-  if (error) {
-      console.log('Error sending message: ', error);
-
-	  res.statusCode = 200;
-			
-      res.setHeader('Content-Type', 'application/json');	
-
-// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
-     var responseBody = {
-        "speech": error,
-        "displayText": error,	     
-        "source": "apiai-Himant-message sample"
-    };
-
+	 });	  
+	 
     res.write(JSON.stringify(responseBody));
     res.end();
-
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-
-	  res.statusCode = 200;
-			
-      res.setHeader('Content-Type', 'application/json');	
-
-// GENERATE THE RESPONSE BODY - HIMANT - And SEND BACK THE RESPONSE TO CLIENT SPEECH Object
-     var responseBody = {
-        "speech": response.body.error,
-        "displayText": response.body.error,	     
-        "source": "apiai-Himant-message sample"
-    };
-
-    res.write(JSON.stringify(responseBody));
-    res.end();
-	  }
-	 else{
-		console.log('Result: '+receiver);
-
-   	      res.statusCode = 200;
-	
-		  res.setHeader('Content-Type', 'application/json');
-	  
-	var responseBody = {
-        "speech": text,
-        "displayText": text,	 
-        "source": "apiai-Himant-OTP sample"
-    };
-
-	
-    res.write(JSON.stringify(responseBody));
-    res.end();
-	
-		 }	 
-  });	  
-	  	  
+	 
 }
 
 
