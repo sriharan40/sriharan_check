f19a29d025703783e58fc1384cc133032a495704
sriharan40 <sriharan40@gmail.com>
2016-10-30 10:10:51 +0530

    Update message.js

--- message.js
+++ message.js
@@ -40,6 +40,21 @@ var options1 = options.split(',');
 
 var mobile = req.params.mobile;
 
+var token = req.params.token;
+
+var crypto = require('crypto');
+var algorithm = 'aes-256-ctr';
+var password = 'd6F3Efeqrts';
+
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+var caller_name = decrypt(token);
+
 var payment = req.params.payment;
 
 var success = req.params.success;
@@ -139,7 +154,7 @@ res.end();
 
 if(mobile)
 {	
-sendMessage(mobile, text1, res);
+sendMessage(mobile, caller_name, text1, res);
 }
 
 function sendNotification(receiver, text, res) {
@@ -293,8 +308,46 @@ messageData1 = {
 }
 
 
-function sendMessage(mobile, text, res) {
+function sendMessage(mobile, caller_name, text, res) {
+
+var db_config = {
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'b213965cc9ad75',
+    password: '9c81ac99',
+    database: 'heroku_a0067bd7c868fc0'
+};
+
+
+var connection;
+
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
 	
+connection.query('SELECT caller_system_name from caller_system where token = '+caller_name+'', function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+		
+var caller_system_name = rows[0].caller_system_name;
+	
+if(caller_system_name)
+{	
 	var accountSid = process.env.accountSid;
 	var authToken = process.env.authToken;
 
@@ -333,6 +386,21 @@ function sendMessage(mobile, text, res) {
         "displayText": text,	 
         "source": "apiai-Himant-OTP sample"
     };
+}
+
+else{
+   var error = "Authentication Error";
+	
+   res.statusCode = 200;
+
+   res.setHeader('Content-Type', 'application/json');
+	  
+	var responseBody = {
+        "speech": error,
+        "displayText": error,	 
+        "source": "apiai-Himant-OTP sample"
+    };	
+}	
 	
     res.write(JSON.stringify(responseBody));
     res.end();
