637cdaebbff73b49e124806ed6d8a67ddb3d4d7f
sriharan40 <sriharan40@gmail.com>
2016-11-11 10:09:59 +0530

    Update sendfbmsg.js

--- modules/sendfbmsg.js
+++ modules/sendfbmsg.js
@@ -1,4 +1,87 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var request = require("request");
+var util = require("util");
+var http = require('http');
+
+var params=function(req){
+  var q=req.url.split('?'),result={};
+  if(q.length>=2){
+      q[1].split('&').forEach((item)=>{
+           try {
+             result[item.split('=')[0]]=item.split('=')[1];
+           } catch (e) {
+             result[item.split('=')[0]]='';
+           }
+      })
+  }
+  return result;
+}
+
+http.createServer(function(req, res) {
+  var headers = req.headers;
+  var method = req.method;
+  var url = req.url;
+  var body = [];
+
+req.params=params(req);
+
+var token = process.env.FB_PAGE_TOKEN;
+
+var sender = req.params.sender;
+
+var options = req.params.options;
+
+if(options)
+{
+var options1 = options.split(',');
+}
+
+if(sender)
+{
+var db_config = {
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'b213965cc9ad75',
+    password: '9c81ac99',
+    database: 'heroku_a0067bd7c868fc0'
+};
+
+
+var connection;
+
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+	
+connection.query('SELECT user_id from t_users where mobile = "'+sender+'"', function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+		
+var sender = rows[0].user_id;
+
+if(sender)
+{
 sendTextMessage(sender, options1, text1, res);
+}
+
+});
 
 function sendTextMessage(sender, options, text, res) {
 
@@ -86,4 +169,6 @@ function sendTextMessage(sender, options, text, res) {
 	}		
   
 	  });
-	  }
+}
+
+}).listen((process.env.PORT), () => console.log("Server listening"));
